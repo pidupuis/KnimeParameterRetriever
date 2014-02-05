08629fdd92c9d9688ee757163c4ea5ef3ec95025
pidupuis <pmj.dupuis@gmail.com>
2014-02-05 22:22:43 +0100

    Extract zip archive

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,12 @@
 package main;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream.GetField;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
 /**
  * 
  * @author pidupuis
@@ -8,7 +15,21 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Initial main");
+		ZipFile zipFile;
+		
+		InputStream in = new Main().getClass().getResourceAsStream("/Exemple.zip");
+		ZipInputStream stream = new ZipInputStream(in);
+		
+		ZipEntry entry;
+        try {
+			while((entry = stream.getNextEntry())!=null) {
+				System.out.println(entry);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
