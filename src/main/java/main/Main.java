fd22a29bf8272477dba12779dbe207f15140ded0
pidupuis <pmj.dupuis@gmail.com>
2014-02-06 21:11:49 +0100

    Store parameters

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -21,6 +24,7 @@ public class Main {
 	static Element racine;
 
 	public static void main(String[] args) {
+		// RETRIEVE ZIP ARCHIVE AS RESOURCES
 		ZipFile zipFile = null;
 		try {
 			zipFile = new ZipFile(new Main().getClass()
@@ -29,47 +33,63 @@ public class Main {
 			e1.printStackTrace();
 		}
 
+		// OPEN THE ZIP ARCHIVE TO RETRIEVE ENTRIES
 		Enumeration<? extends ZipEntry> entries = zipFile.entries();
-
 		while (entries.hasMoreElements()) {
 			ZipEntry entry = entries.nextElement();
-
-			if (entry.getName().contains("workflow.knime")) {
-				// System.out.println(entry);
+			// SEARCH THE FILE CONTAINING THE PARAMETERS
+			if (entry.getName().contains("workflow.knime")) { 
+				
+				// OPEN THE FILE AS XML FILE
 				SAXBuilder sxb = new SAXBuilder();
 				try {
 					document = sxb.build(zipFile.getInputStream(entry));
 				} catch (JDOMException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				
+				// PREPARE THE VARIABLE TO STORE THE PARAMETERS
+				LinkedHashMap<String, ArrayList<String>> parameters = new LinkedHashMap<String, ArrayList<String>>();
 
+				// PARSE THE XML FILE
 				racine = document.getRootElement();
-
 				List<Element> configs = racine.getChildren();
 				for (Element e : configs) {
+					// RETRIEVE THE WORKFLOWS VARIABLES
 					if (e.getAttributeValue("key").equalsIgnoreCase(
 							"workflow_variables")) {
-						System.out.println(e.getAttributeValue("key"));
 						List<Element> variables = e.getChildren();
 						for (Element f : variables) {
-							System.out.println("###");
-							System.out.println(f.getAttributeValue("key"));
+							// RETRIEVE THE PARAMETERS COMPONENTS
 							List<Element> values = f.getChildren();
+							String name = new String();
+							String type = new String();
+							String value = new String();
 							for (Element g : values) {
-								System.out.println(g.getAttributeValue("key"));
-								System.out.println(g.getAttributeValue("type"));
-								System.out.println(g.getAttributeValue("value")
-										+ "\n");
+								if (g.getAttributeValue("key").equalsIgnoreCase("name")) {
+									name = g.getAttributeValue("value");
+								}
+								if (g.getAttributeValue("key").equalsIgnoreCase("class")) {
+									type = g.getAttributeValue("value");
+								}
+								if (g.getAttributeValue("key").equalsIgnoreCase("value")) {
+									value = g.getAttributeValue("value");
+								}
+								
+								ArrayList<String> temp = new ArrayList<String>();
+								temp.add(type);
+								temp.add(value);
+								parameters.put(name, temp);
 							}
 						}
+						break;
 					}
 				}
-
+				System.out.println(parameters);
 			}
+			break;
 		}
 
 	}
